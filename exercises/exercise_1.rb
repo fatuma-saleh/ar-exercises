315ac2b83d42fbf8caaf48c39696595f906fb7bf
Fatuma <sfatex@gmail.com>
2022-01-06 16:16:00 -0500

    Completed exercise2 ,updated name of store

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,10 +3,10 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 # create Burnaby (annual_revenue of 300000, carries men's and women's apparel)
- store1 = Store.create(annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+ store1 = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
 #create Richmond (annual_revenue of 1260000 carries women's apparel only)
-store2 = Store.create(annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+store2 = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
 # create Gastown (annual_revenue of 190000 carries men's apparel only)
-store3 = Store.create(annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+store3 = Store.create(name: "Gastown",annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
 puts Store.count();
 # Your code goes below here ...
--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -3,5 +3,10 @@ require_relative './exercise_1'
 
 puts "Exercise 2"
 puts "----------"
+@store1 = Store.find(1)
+@store2 = Store.find(2)
 
+ @store1.name = "fatuma"
+@store1.save;
+puts @store2
 # Your code goes here ...
