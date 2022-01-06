35bf6eab32e7e893ec2959f89df4a4e215a961d2
Fatuma <sfatex@gmail.com>
2022-01-06 17:17:36 -0500

    Completed exercise6 ,created relationships and added emplyees to stores

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,7 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store2.employees.create(first_name: "Asu", last_name: "Semir", hourly_rate: 50)
+@store1.employees.create(first_name: "Fatuma", last_name: "Saleh", hourly_rate: 80)
+@store2.employees.create(first_name: "Laila", last_name: "Negash", hourly_rate: 60)
